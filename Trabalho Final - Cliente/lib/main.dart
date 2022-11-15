40ae67e05446eabaf8be3ca99e7a711e6781385e
VitoriaMarcia2 <117932268+VitoriaMarcia2@users.noreply.github.com>
2022-11-15 17:39:26 -0300

    Add files via upload

--- Trabalho Final - Cliente/lib/main.dart
+++ Trabalho Final - Cliente/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.indigo,
       ),
       routes: routes(),
     );
--- Trabalho Final - Cliente/lib/routes.dart
+++ Trabalho Final - Cliente/lib/routes.dart
@@ -6,6 +6,12 @@ import 'pages/cupons_page.dart';
 import 'pages/cartoes_page.dart';
 import 'pages/pedidos_page.dart';
 import 'pages/home_page.dart';
+import 'pages/refrigerante.dart';
+import 'pages/cerveja.dart';
+import 'pages/vinho.dart';
+import 'pages/espumantes.dart';
+import 'pages/suco.dart';
+import 'pages/energetico.dart';
 
 Map<String, WidgetBuilder> routes(){
   return{
@@ -13,5 +19,13 @@ Map<String, WidgetBuilder> routes(){
     '/cupons': (context) => CuponsPage(),
     '/cartoes': (context) => CartoesPage(),
     '/pedidos': (context) =>PedidosPage(),
+    '/refrigerante' : (context) => RefrigerantePage(),
+    '/cerveja' : (context) => CervejaPage(),
+    '/vinho' : (context) => VinhoPage(),
+    '/suco' : (context) => SucosPage(),
+    '/energetico' : (context) => EnergeticoPage(),
+    '/espumante' : (context) => EspumantesPage(),
+
+
   };
 }
